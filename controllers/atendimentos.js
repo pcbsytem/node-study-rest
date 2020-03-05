4538b385e9c7525a0fec411485470c3349a0fd8c
pcbsytem <pcbsytem@hotmail.com>
2020-03-05 07:34:56 -0300

    Include body-parse

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -3,7 +3,8 @@ module.exports = (app) => {
     res.send('Voce esta na rota de atendimento e esta realizando um get!')
   );
 
-  app.post('/atendimentos', (req, res) =>
+  app.post('/atendimentos', (req, res) => {
+    console.log(req.body);
     res.send('Voce esta na rota de atendimentos e esta realizando um post.')
-  );
+  });
 }
