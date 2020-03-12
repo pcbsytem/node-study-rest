93d5c33bc50e9ced91f702dea7ff91664119b404
pcbsytem <pcbsytem@hotmail.com>
2020-03-12 07:42:37 -0300

    Add validation, get list and get by id

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,14 +1,19 @@
 const Atendimento = require('../models/atendimentos');
 
 module.exports = (app) => {
-  app.get('/atendimentos', (req, res) =>
-    res.send('Voce esta na rota de atendimento e esta realizando um get!')
-  );
+  app.get('/atendimentos', (req, res) => {
+    Atendimento.lista(res);
+  });
+
+  app.get('/atendimentos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    Atendimento.buscaPorId(id, res);
+  })
 
   app.post('/atendimentos', (req, res) => {
     const atendimento = req.body;
 
-    Atendimento.adiciona(atendimento);
-    res.send('Post Atendimento')
+    Atendimento.adiciona(atendimento, res);
   });
 }
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -2,18 +2,69 @@ const moment = require('moment');
 const conexao = require('../infraestrutura/conexao');
 
 class Atendimento {
-  adiciona(atendimento) {
+  adiciona(atendimento, res) {
     const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
     const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+
+    const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+    const clientEhValido = atendimento.cliente.length >= 5;
+
+    const validacoes = [
+      {
+        nome: 'data',
+        valido: dataEhValida,
+        mensagem: 'Data deve ser maior ou igual atual'
+      },
+      {
+        nome: 'cliente',
+        valido: clientEhValido,
+        mensagem: 'Cliente deve ter pelo menos 5 caracteres'
+      }
+    ];
+
+    const erros = validacoes.filter(campo => !campo.valido);
+    const existemErros = erros.length;
+
+    if (existemErros) {
+      return res.status(400).json(erros);
+    }
+
     const atendimentoDatado = { ...atendimento, dataCriacao, data };
 
     const sql = 'INSERT INTO Atendimentos SET ?';
 
     conexao.query(sql, atendimentoDatado, (erro, resultados) => {
       if (erro) {
+        res.status(400).json(erro);
         console.log(erro);
       } else {
-        console.log(resultados);
+        res.status(201).json(resultados);
+      }
+    })
+  }
+
+  lista(res) {
+    const sql = 'SELECT * FROM Atendimentos';
+
+    conexao.query(sql, (erro, resultados) => {
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json(resultados);
+      }
+    })
+  }
+
+  buscaPorId(id, res) {
+    const sql = `SELECT * FROM Atendimentos WHERE id=${id}`;
+
+    conexao.query(sql, (erro, resultados) => {
+      const atendimento = resultados[0];
+
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json(atendimento);
       }
     })
   }
